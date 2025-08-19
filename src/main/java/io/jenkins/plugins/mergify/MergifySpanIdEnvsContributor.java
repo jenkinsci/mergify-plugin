2333a1aaeaec07a6867c82ef6a19887f8f634d3b
Mehdi ABAAKOUK <sileht@sileht.net>
2025-08-19 23:10:11 +0200

    fix: move from EnvironmentContributor to StepEnvironmentContributor
    This ensures the MERGIFY_TRACEPARENT when multiple step run in parallel.

--- src/main/java/io/jenkins/plugins/mergify/MergifySpanIdEnvsContributor.java
+++ src/main/java/io/jenkins/plugins/mergify/MergifySpanIdEnvsContributor.java
@@ -2,15 +2,24 @@ package io.jenkins.plugins.mergify;
 
 import hudson.EnvVars;
 import hudson.Extension;
-import hudson.model.EnvironmentContributor;
-import hudson.model.Run;
 import hudson.model.TaskListener;
+import java.io.IOException;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepEnvironmentContributor;
 
 @Extension
-public class MergifySpanIdEnvsContributor extends EnvironmentContributor {
+public class MergifySpanIdEnvsContributor extends StepEnvironmentContributor {
 
-    public void buildEnvironmentFor(Run run, EnvVars envs, TaskListener listener) {
-        ParentSpanAction action = run.getAction(ParentSpanAction.class);
+    public void buildEnvironmentFor(StepContext stepContext, EnvVars envs, TaskListener listener) {
+        FlowNode node;
+        try {
+            node = stepContext.get(FlowNode.class);
+        } catch (IOException | InterruptedException e) {
+            return;
+        }
+
+        ParentSpanAction action = node.getAction(ParentSpanAction.class);
         if (action != null) {
             envs.put("MERGIFY_TRACEPARENT", action.getTraceParent());
         }
